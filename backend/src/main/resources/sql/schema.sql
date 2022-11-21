6594ea649575faec872a3fb4b2591027bfb0d73f
tercanfurkan <tercanfurkan@gmail.com>
2022-11-22 00:02:12 +0200

    Correct db schema

--- backend/src/main/resources/sql/schema.sql
+++ backend/src/main/resources/sql/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS messages
     id                     VARCHAR(60) DEFAULT RANDOM_UUID() PRIMARY KEY,
     content                VARCHAR      NOT NULL,
     content_type           VARCHAR(128) NOT NULL,
-    sent_at                TIMESTAMP    NOT NULL,
+    sent                   TIMESTAMP    NOT NULL,
     username               VARCHAR(60)  NOT NULL,
-    user_avatar_link       VARCHAR(256) NOT NULL
+    user_avatar_image_link VARCHAR(256) NOT NULL
 );
